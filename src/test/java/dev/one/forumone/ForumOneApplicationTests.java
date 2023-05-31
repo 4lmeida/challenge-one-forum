8cbbd969b7fc1fd431c25b462b891cd65b94b4de
Luis <lferreira646@gmail.com>
2023-05-31 16:21:55 -0300

    projeto criado

---
+++ src/test/java/dev/one/forumone/ForumOneApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.one.forumone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
